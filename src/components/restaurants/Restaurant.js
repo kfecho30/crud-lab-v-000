6a14bdd1a19b896088cdb4843fd2215a106ca5a5
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 17:03:54 +0000

    automatically backed up by learn

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -3,7 +3,7 @@ import ReviewsContainer from '../../containers/ReviewsContainer'
 
 class Restaurant extends Component {
   handleClick = () => {
-    this.props.deleteRestaurant(this.props.restaurant.id);
+    return this.props.deleteRestaurant(this.props.restaurant.id);
   }
 
   render() {
