efa00ab294090dcefb8f9a041400256f4889aa9f
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 17:42:05 +0000

    automatically backed up by learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -1,27 +1,37 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state = {restaurants:[], reviews:[]}, action) {
-  switch(action.type){
-    case "ADD_RESTAURANT":
-      const restaurant = {
-        id: cuidFn(),
-        text: action.text
+export default function manageRestaurants(state = {
+  restaurants: [],
+  reviews: [],
+}, action) {
+  switch (action.type) {
+
+    case 'ADD_RESTAURANT':
+
+      const restaurant = { text: action.text, id: cuidFn() };
+      return {
+        ...state,
+        restaurants: [ ...state.restaurants, restaurant]
       }
-      return {restaurants: state.restaurants.concat(restaurant)}
-    case "DELETE_RESTAURANT":
-      return {restaurants: state.restaurants.filter(restaurant => restaurant.id !== action.id)}
-      case 'ADD_REVIEW':
 
-        const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
-        return { ...state,
-          reviews: [...state.reviews, review]
-        }
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return { ...state, restaurants}
 
-      case 'DELETE_REVIEW':
-        const reviews = state.reviews.filter(review => review.id !== action.id);
-        return {...state, reviews }
+    case 'ADD_REVIEW':
+
+      const review = { text: action.review.text, restaurantId: action.review.restaurantId, id: cuidFn() };
+      return { ...state,
+        reviews: [...state.reviews, review]
+      }
+
+    case 'DELETE_REVIEW':
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return {...state, reviews }
+
     default:
       return state;
+
   }
-}
+};
