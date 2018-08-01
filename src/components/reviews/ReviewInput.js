9d40aa87b01123902c5339c9b51d76fbba2df58a
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 17:18:44 +0000

    automatically backed up by learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,6 +2,14 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state={
+      text:'',
+    }
+  }
+  
   render() {
     return (
       <div>
