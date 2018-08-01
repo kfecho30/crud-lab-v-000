5a88e06c25504cfc5df9e98eaf2eb68c78dbbe5d
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 17:29:40 +0000

    automatically backed up by learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -4,6 +4,7 @@ import Review from './Review';
 class Reviews extends Component {
   render() {
     const {reviews, restaurantId, deleteReview} = this.props
+    const assoc = reviews.filter(review => review.restaurantId === restaurantId)
     const reviewList = reviews.map(review => {
       return (
         <Review key={review.id} review={review} deleteReview={deleteReview} />
