c8bddbceefe615317d7ff643e63437471deeb5e7
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 17:38:00 +0000

    automatically backed up by learn

--- src/components/reviews/Review.js
+++ src/components/reviews/Review.js
@@ -4,8 +4,8 @@ class Review extends Component {
   handleClick = () => {
     this.props.deleteReview(this.props.review.id);
   }
-  render() {
 
+  render() {
     return (
       <div>
         <li>
