3ef397750ce3bb6eb503552d6102293b01a80e11
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 16:48:36 +0000

    automatically backed up by learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -5,5 +5,7 @@ export default function manageRestaurants(state, action) {
   switch(action.type){
     case "ADD_RESTAURANT":
       return {state.restaurants.concat(action.text)}
+    default:
+      return state;
   }
 }
