021e4ec91da4f593c2b5e2ceaefd8ea5d9433271
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 16:48:25 +0000

    automatically backed up by learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,5 +2,8 @@ import cuid from 'cuid';
 export const cuidFn = cuid;
 
 export default function manageRestaurants(state, action) {
-
+  switch(action.type){
+    case "ADD_RESTAURANT":
+      return {state.restaurants.concat(action.text)}
+  }
 }
