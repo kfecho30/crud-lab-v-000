562fb1540fb628b9acd91c21a469f8bcb61f68cf
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 16:46:23 +0000

    automatically backed up by learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -19,7 +19,7 @@ class RestaurantInput extends Component {
   render() {
     return (
       <div>
-        <form onSubmit={}>
+        <form onSubmit={this.handleSubmit}>
           <input type="text" onChange={this.handleChange} value={this.state.text}/>
           <input type="submit"/>
         </form>
