2275aadb13719cf4dca9661e60842fc8dbeff2b4
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 17:33:14 +0000

    automatically backed up by learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -6,7 +6,7 @@ class Reviews extends Component {
   render() {
 
     const { reviews, restaurantId, deleteReview } = this.props;
-    const associatedReviews = reviews.filter(review => review.restaurantId !== restaurantId);
+    const associatedReviews = reviews.filter(review => review.restaurantId === restaurantId);
 
     const reviewList = associatedReviews.map((review, index) => {
       return <Review key={index} review={review} deleteReview={deleteReview} />
