892c3d220e2b78f79f3bf4747a09e95d3c775ba8
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 17:04:42 +0000

    automatically backed up by learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 class Restaurants extends Component {
   render() {
+    const {restaurants, deleteRestaurant} = this.props
     return(
       <ul>
         Restaurants Component
