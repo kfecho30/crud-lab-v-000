5c6ef47a1576f60bb40a1d9f873bc883feab19b9
Kelsey Fecho <kfecho30@gmail.com>
2018-08-01 17:29:02 +0000

    automatically backed up by learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,7 +3,7 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
-    const {reviews, deleteReview} = this.props
+    const {reviews, restaurantId, deleteReview} = this.props
     const reviewList = reviews.map(review => {
       return (
         <Review key={review.id} review={review} deleteReview={deleteReview} />
